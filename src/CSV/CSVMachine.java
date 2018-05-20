d41472644b82b77231563308de05e7e18ff15ff2
tom-nash <thomas.nash@connect.qut.edu.au>
2018-05-20 13:48:00 +1000

    Developing CSV Features

--- src/CSV/CSVMachine.java
+++ src/CSV/CSVMachine.java
@@ -4,9 +4,24 @@
 package CSV;
 
 /**
- * @author John_
+ *This class provides CSV features to the project
  *
+ * @author Tom
  */
 public class CSVMachine {
-
+	public String readItemProperties() {
+		return null;
+	}
+	
+	public String writeManifest() {
+		return null;
+	}
+	
+	public String readManifest() {
+		return null;
+	}
+	
+	public String readSalesLog() {
+		return null;
+	}
 }
