8d55cb71c4a8eaaa31baa6cfd35a3933e33df524
John Huynh <john.huynh@student.qut.edu.au>
2018-05-21 17:14:02 +1000

    added GUI

--- src/GUI/GUIApplication.java
+++ src/GUI/GUIApplication.java
@@ -1,9 +1,186 @@
+/**
+ * 
+ */
 package GUI;
 
-public class GUIApplication {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GraphicsConfiguration;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
-	public GUIApplication() {
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import CSV.CSVMachine;
+import Exception.CSVFormatException;
+
+/**
+ * @author John Huynh
+ *
+ */
+public class GUIApplication extends JFrame implements ActionListener, Runnable{
+	
+	public static final int WIDTH = 700;
+	public static final int HEIGHT = 900;
+	
+	private JPanel pnlDisplay;
+	
+	private JButton btnExtractMan;
+	private JButton btnLoadMan;
+	private JButton btnUpdateItems;
+	private JPanel pnlBtn;
+	private JPanel pnlTop;
+	private JLabel storeNameLabel;
+	private JLabel capitalTextLabel;
+	private JLabel capitalValueLabel;
+	
+	/**
+	 * @param arg0
+	 * @throws HeadlessException
+	 */
+	public GUIApplication(String arg0) throws HeadlessException {
+		super(arg0);
 		// TODO Auto-generated constructor stub
+	}
+	
+	private void createGUI() {
+		setSize(WIDTH, HEIGHT);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setLayout(new BorderLayout());
+	    
+	    pnlDisplay = createPanel(Color.BLACK);
+	    
+	    pnlBtn = createPanel(Color.WHITE);
+	    
+	    pnlTop = createPanel(Color.WHITE);
+	    
+	    btnExtractMan = createButton("Extract Manifest");
+	    btnLoadMan = createButton("Load Manifest");
+	    btnUpdateItems = createButton("Update Items");
+	    
+	    pnlDisplay.setLayout(new BorderLayout());
+	 
+	    layoutButtonPanel(); 
+	    layoutTextPanel();
+	    
+	    this.getContentPane().add(pnlDisplay,BorderLayout.CENTER);
+	    this.getContentPane().add(pnlTop,BorderLayout.NORTH);
+	    this.getContentPane().add(pnlBtn,BorderLayout.SOUTH);
+	    repaint(); 
+	    this.setVisible(true);
+	}
+	//Create JPannel
+	private JPanel createPanel(Color c) {
+		JPanel jp = new JPanel();
+		jp.setBackground(c);
+		return jp;
+	}
+	
+	//Create JButton
+	private JButton createButton(String str) {
+		JButton jb = new JButton(str);
+		jb.addActionListener(this);
+		return jb;
+	}
+	
+	private void layoutButtonPanel() {
+		GridBagLayout layout = new GridBagLayout();
+	    pnlBtn.setLayout(layout);
+	    
+	    //add components to grid
+	    GridBagConstraints constraints = new GridBagConstraints(); 
+	    
+	    //Defaults
+	    constraints.fill = GridBagConstraints.NONE;
+	    constraints.anchor = GridBagConstraints.CENTER;
+	    constraints.weightx = 100;
+	    constraints.weighty = 100;
+	    
+	    addToPanel(pnlBtn, btnExtractMan,constraints,0,0,2,1); 
+	    addToPanel(pnlBtn, btnLoadMan,constraints,2,0,2,1); 
+	    addToPanel(pnlBtn, btnUpdateItems,constraints,4,0,2,1); 	
+	}
+	
+	private void layoutTextPanel() {
+		GridBagLayout layout = new GridBagLayout();
+		pnlTop.setLayout(layout);
+		
+		GridBagConstraints constraints = new GridBagConstraints();
+		 //Defaults
+	    constraints.fill = GridBagConstraints.NONE;
+	    constraints.anchor = GridBagConstraints.LINE_START;
+	    constraints.weightx = 1;
+	    constraints.weighty = 1;
+	    
+	    storeNameLabel = new JLabel("Store Name");
+	    capitalTextLabel = new JLabel("Store Capital:");
+	    constraints.anchor = GridBagConstraints.CENTER;
+	    capitalValueLabel = new JLabel("0");
+	    addToPanel(pnlTop,storeNameLabel,constraints,5,0,10,1);
+	    addToPanel(pnlTop,capitalTextLabel,constraints,3,1,10,1);
+	    addToPanel(pnlTop,capitalValueLabel,constraints,10,1,10,1);
+	    
+	}
+	
+	
+	/**
+     * 
+     * A convenience method to add a component to given grid bag
+     * layout locations.
+     *
+     * @param c the component to add
+     * @param constraints the grid bag constraints to use
+     * @param x the x grid position
+     * @param y the y grid position
+     * @param w the grid width
+     * @param h the grid height
+     */
+   private void addToPanel(JPanel jp,Component c, GridBagConstraints constraints, int x, int y, int w, int h) {  
+      constraints.gridx = x;
+      constraints.gridy = y;
+      constraints.gridwidth = w;
+      constraints.gridheight = h;
+      jp.add(c, constraints);
+   }
+
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		createGUI(); 
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFrame.setDefaultLookAndFeelDecorated(true);
+        SwingUtilities.invokeLater(new GUIApplication("BorderLayout"));
+        
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
